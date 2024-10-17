c6dc268b6e453a19effc7ea07cd51d759ddf5900
odoodjinn <odoodjinn@gmail.com>
2024-10-17 15:12:15 +0530

    oct17

--- employee_dashboard/models/hr_employee.py
+++ employee_dashboard/models/hr_employee.py
@@ -12,34 +12,20 @@ class HrEmployee(models.Model):
     def get_tiles_data(self):
         """ Return the tile data"""
         user_id = self.env.user
-        employee_data = self.sudo().search([])
         employee_attendance = self.env['hr.attendance'].sudo().search([])
         employee_leave = self.env['hr.leave'].sudo().search([])
         employee_project = self.env['project.project'].sudo().search([])
-        employee_self = employee_data.filtered(lambda x: x.user_id == user_id)
         attendance = employee_attendance.filtered(lambda x: x.employee_id.user_id == user_id)
         leave = employee_leave.filtered(lambda x: x.employee_id.user_id == user_id)
         project = employee_project.filtered(lambda x: x.user_id == user_id)
-        today = fields.Datetime.today()
-        experience = (today-employee_self.create_date).days/365
-        print(employee_self.create_date,'//selfprint')
-        employee_info = {
-            'id': employee_self.id,
-            'name': employee_self.name,
-            'position': employee_self.job_id.name,
-            'department': employee_self.department_id.name,
-            'experience': int(experience),
-
-        }
         return {
-            'user_id': user_id,
-            'total_employee': len(employee_data),
             'total_attendance': len(attendance),
             'total_leave': len(leave),
             'total_project': len(project),
-            'employee_info': employee_info,
         }
 
     def _compute_experience(self):
+        """compute the experience of the employee using the create_date of the record on model"""
+        today = fields.Datetime.today()
         for rec in self:
-            rec.experience = (fields.Datetime.today()-rec.create_date).days/365
+            rec.experience = (today-rec.create_date).days/365
--- employee_dashboard/static/src/js/dashboard.js
+++ employee_dashboard/static/src/js/dashboard.js
@@ -3,7 +3,6 @@ import { registry } from "@web/core/registry";
 import { useState, Component, onWillStart } from  "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { user } from "@web/core/user";
-import { renderToFragment } from "@web/core/utils/render";
 
 const actionRegistry = registry.category("actions");
 class EmployeeDashboard extends Component {
@@ -11,20 +10,22 @@ class EmployeeDashboard extends Component {
          super.setup()
          this.orm = useService('orm')
          this.action = useService("action")
-         this.orm = useService("orm")
          this.state = useState({
             value: "",
             user_name: "",
             user_val: "",
             user_job: "",
             emp_value: "",
+            emp_user_id: user.userId,
+            emp_id: "",
         })
          onWillStart(async () => {
-            this.isEmployeeManager = await user.hasGroup("base.group_system");
+            this.isEmployeeManager = await user.hasGroup("hr.group_hr_manager");
          });
          this._fetch_data()
     }
     async _fetch_data(){
+    // fetch data to the dashboard
         this.state.value = await this.orm.searchRead("hr.employee", [], [])
         var obj = await this.state.value
         var user_data = []
@@ -36,78 +37,117 @@ class EmployeeDashboard extends Component {
             }
         }
         this.state.user_val = user_data[0]
+        var employee_id = user_data[0].id
+        this.state.emp_id = parseInt(employee_id)
         this.state.user_name = user_data[0].display_name
         this.state.user_job = user_data[0].job_id[1]
+        document.getElementById('experience').innerText = 'Experience: ' + user_data[0].experience + ' years';
         var child_array = child_list[0]
         for (let i=0; i < child_array.length; i++){
             var child_data = await this.orm.searchRead("hr.employee", [["id", "=", child_array[i]]], [])
             document.getElementById('chart_list').innerHTML +=(`<li class="o_org_chart_entry o_org_chart_entry_sub o_treeEntry d-flex position-relative py-2 overflow-visible">` + child_data[0].display_name + `</li>`);
         }
         this.orm.call("hr.employee", "get_tiles_data", [], {}).then(function(result){
-        document.getElementById('experience').append('Experience: ' + result.employee_info.experience + ' years');
-        document.getElementById('attendance').append(result.total_attendance);
-        document.getElementById('leaves').append(result.total_leave);
-        document.getElementById('project').append(result.total_project);
+        document.getElementById('attendance').innerText = result.total_attendance;
+        document.getElementById('leaves').innerText = result.total_leave;
+        document.getElementById('project').innerText = result.total_project;
         });
    };
    attendanceTileClick(){
-        this.action.doAction({
-                type: 'ir.actions.act_window',
-                res_model: 'hr.attendance',
-                domain: [["employee_id.user_id", "=", user.userId]],
-                views: [[false, 'list'], [false, 'form']],
-                target: 'current',
-            })
+   // Click function on attendance tile to display the attendance records of the user/selected employee
+       this.action.doAction({
+            type: 'ir.actions.act_window',
+            res_model: 'hr.attendance',
+            domain: [["employee_id", "=", this.state.emp_id]],
+            views: [[false, 'list'], [false, 'form']],
+            target: 'current',
+       })
    }
    leaveTileClick(){
+  // Click function on leave tile to display the time-off records of the user/selected employee
         this.action.doAction({
-                type: 'ir.actions.act_window',
-                res_model: 'hr.leave',
-                domain: [["employee_id.user_id", "=", user.userId]],
-                views: [[false, 'list'], [false, 'form']],
-                target: 'current',
-            })
+            type: 'ir.actions.act_window',
+            res_model: 'hr.leave',
+            domain: [["employee_id", "=", this.state.emp_id]],
+            views: [[false, 'list'], [false, 'form']],
+            target: 'current',
+        })
    }
    projectTileClick(){
+  // Click function on project tile to display the project records of the user/selected employee
         this.action.doAction({
                 type: 'ir.actions.act_window',
                 res_model: 'project.project',
-                domain: [["user_id", "=", user.userId]],
+                domain: [["user_id", "=", this.state.emp_user_id]],
                 views: [[false, 'list'], [false, 'form']],
                 target: 'current',
             })
    }
-   personalInfoTileClick(){
+   personalInfoTileClick(e){
+  // Click function on personal information tile to display the form view of the user/selected employee
         var action_employee = this.action
-        this.orm.call("hr.employee", "get_tiles_data", [], {}).then(function(result){
-            var eid = result.employee_info['id']
-            action_employee.doAction({
-                type: 'ir.actions.act_window',
-                res_model: 'hr.employee',
-                res_id: eid,
-                views: [[false, "form"]],
-                target: 'current',
-            })
-        })
-   }
-   orgChartClick(){
-        this.action.doAction({
-                type: 'ir.actions.act_window',
-                res_model: 'hr.employee',
-                views: [[false, 'hierarchy']],
-                target: 'current',
+        action_employee.doAction({
+            type: 'ir.actions.act_window',
+            res_model: 'hr.employee',
+            res_id: this.state.emp_id,
+            views: [[false, "form"]],
+            target: 'current',
         })
    }
    async empClick(e){
+  // Click function from the list of employees only visible to the 'hr.group_hr_manager' to
+  // display the selected employee data
         var target = e.target
-        document.getElementById('experience').style.display= "none";
-//        document.getElementById("dashboard").style.display = "none";
-        console.log(e)
         var emp_id = target.children[0].innerText;
+        var emp_user_id = target.children[1].innerText
+        this.state.emp_id = parseInt(emp_id)
+        this.state.emp_user_id = parseInt(emp_user_id);
         this.state.emp_value = await this.orm.searchRead("hr.employee", [["id", "=", emp_id]], [])
+
+        // calculate attendance count
+        var emp_attendance = await this.orm.searchRead("hr.attendance", [], [])
+        var att_data = []
+        for (let i=0; i<emp_attendance.length; i++){
+            if (emp_attendance[i].employee_id[0] == emp_id){
+                att_data.push(emp_attendance[i])
+            }
+        }
+        document.getElementById('attendance').innerText = att_data.length;
+
+        // calculate leave count
+        var leave = await this.orm.searchRead("hr.leave", [], ["id","employee_id"])
+        var leave_data = []
+        for (let i=0; i<leave.length; i++){
+            if (leave[i].employee_id[0] == emp_id){
+                leave_data.push(leave[i])
+            }
+        }
+        document.getElementById('leaves').innerText = leave_data.length;
+
+        // calculate project count
+        var project = await this.orm.searchRead("project.project", [], [])
+        var project_data = []
+        for (let i=0; i<project.length; i++){
+            if (project[i].user_id[0] == emp_user_id){
+                project_data.push(project[i])
+            }
+        }
+        document.getElementById('project').innerText = project_data.length;
+
+        // Displays personal information and employee hierarchy based on the employee selection
         this.state.user_name = this.state.emp_value[0].display_name
         this.state.user_job = this.state.emp_value[0].job_id[1]
-        console.log(this.state.emp_value,'//val')
+        document.getElementById("experience").innerText = 'Experience: ' + this.state.emp_value[0].experience + ' years'
+        var child_val = this.state.emp_value[0].child_ids
+        this.state.user_val = this.state.emp_value[0]
+        document.getElementById("chart_list").style.display = "none";
+        document.getElementById("emp_chart_list").innerHTML = ""
+        if (child_val.length >= 1){
+            for (let i=0; i<child_val.length; i++){
+                var emp_child = await this.orm.searchRead("hr.employee", [["id", "=", child_val[i]]], [])
+                document.getElementById("emp_chart_list").innerHTML +=(`<li class="o_org_chart_entry o_org_chart_entry_sub o_treeEntry d-flex position-relative py-2 overflow-visible">` + emp_child[0].display_name + `</li>`);
+            }
+        }
    }
 }
 EmployeeDashboard.template = "employee_dashboard.EmployeeDashboard";
