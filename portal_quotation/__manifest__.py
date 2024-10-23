b355dbb52ece233f97d083e7bcc616c443d68b39
odoodjinn <odoodjinn@gmail.com>
2024-10-23 14:22:08 +0530

    oct23

---
+++ portal_quotation/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Portal Quotation',
+    'version': '18.0.1.0.0',
+    'category': 'Sale',
+    'summary': 'Displays quotation on customer portal',
+    'description': """
+        This module helps the portal users to view their sale quotation on customer portal.
+    """,
+    'depends': ['base', 'sale', 'web'],
+    'data': [
+        'views/quotation_portal_templates.xml',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ portal_quotation/controller/portal.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo.addons.portal.controllers.portal import CustomerPortal
+
+from odoo import http
+from odoo.http import request
+
+
+class CustomerPortalQuote(CustomerPortal):
+    @http.route(['/my/orders/<int:order_id>/confirm'], type='http', auth="public", methods=['POST'], website=True)
+    def portal_quote_confirm(self, order_id, access_token=None):
+        """function working on confirm button"""
+        order_sudo = self._document_check_access('sale.order', order_id, access_token=access_token)
+        order_sudo.action_confirm()
+        # redirect url to show confirm message on the window
+        redirect_url = order_sudo.get_portal_url(query_string="&message=sign_ok")
+        return request.redirect(redirect_url)
